e4dc74188418f6f7ce16ccbeac57a088ea821a66
Mostafa54225 <mostafa.draven@gmail.com>
2021-08-30 21:48:45 +0200

    Fix Some Bugs

--- client/src/Components/Navbar/NavbarUser.js
+++ client/src/Components/Navbar/NavbarUser.js
@@ -1,26 +1,46 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
 
 import './Navbar.css'
 
+  
+
 const NavbarUser = () => {
 
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector(".nav-menu");
+  const navLink = document.querySelectorAll(".nav-link");
+
+  function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  }
+
+  navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+  function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  }
+
   return (
-    <nav>
-      <NavLink to="/" className="header"> Home </NavLink>
-      <ul className="navbar-links">
-        <li>
-        <NavLink to="/Voting" activeClassName="nav-active">
-            <i className="far fa-vote-yea" /> Voting
-          </NavLink>
-        </li>
-        <li>
-        <NavLink to="/Result" activeClassName="nav-active">
-            <i className="far fa-poll-h" /> Results
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+    <header className="header">
+        <nav className="navbar">
+            <a href="/" className="nav-logo">Home.</a>
+            <ul className="nav-menu">
+                <li className="nav-item">
+                    <a href="/Voting" className="nav-link">Voting</a>
+                </li>
+                <li className="nav-item">
+                    <a href="/Result" className="nav-link">Result</a>
+                </li>
+            </ul>
+            <div className="hamburger" onClick={mobileMenu}>
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+            </div>
+        </nav>
+</header>
   )
 }
 
--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,5 @@
 const path = require('path')
 module.exports = {
-  /**
-   * Networks define how you connect to your ethereum client and let you set the
-   * defaults web3 uses to send transactions. If you don't specify one truffle
-   * will spin up a development blockchain for you on port 9545 when you
-   * run `develop` or `test`. You can ask a truffle command to use a specific
-   * network from the command line, e.g
-   *
-   * $ truffle test --network <network-name>
-   */
 
   networks: {
     // Useful for testing. The `development` name is special - truffle uses it by default
@@ -70,17 +61,6 @@ module.exports = {
       // }
     }
   },
-
-  // Truffle DB is currently disabled by default; to enable it, change enabled:
-  // false to enabled: true. The default storage location can also be
-  // overridden by specifying the adapter settings, as shown in the commented code below.
-  //
-  // NOTE: It is not possible to migrate your contracts to truffle DB and you should
-  // make a backup of your artifacts to a safe location before enabling this feature.
-  //
-  // After you backed up your artifacts you can utilize db by running migrate as follows: 
-  // $ truffle migrate --reset --compile-all
-  //
   // db: {
     // enabled: false,
     // host: "127.0.0.1",
